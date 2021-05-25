0640ba56b94212fabc0c49117822b1f34117e9bc
ahsan-raazaa <39822331+ahsan-raazaa@users.noreply.github.com>
2021-05-25 22:43:14 +0500

    Stack peek operations

---
+++ 31_stack_op2ll.c
@@ -0,0 +1,101 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node* next;
+};
+
+struct Node* top = NULL;
+
+int isEmpty(struct Node* ptr)
+{
+    if(top == NULL)
+    {
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+int isFull(struct Node* top)
+{
+    struct Node* p = (struct Node*)malloc(sizeof(struct Node));
+    if(p == NULL)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+struct Node* push(struct Node* top, int x){
+    if(isFull(top)){
+        printf("Stack Overflow\n");
+    }
+    else{
+        struct Node* n = (struct Node*) malloc(sizeof(struct Node));
+        n->data = x;
+        n->next = top;
+        top = n;
+        return top;
+    }
+}
+
+int pop(struct Node* tp){
+    if(isEmpty(tp)){
+        printf("Stack Underflow\n");
+    }
+    else{
+        struct Node* n = tp;
+        top = (tp)->next;
+        int x = n->data;
+        free(n);
+        return x; 
+    }
+}
+
+
+void linkedListTraversal(struct Node *ptr)
+{
+    while (ptr != NULL)
+    {
+        printf("Element: %d\n", ptr->data);
+        ptr = ptr->next; 
+    }
+}
+
+int peek(int pos)
+{
+    struct Node* ptr = top;
+    for(int i = 0; i < pos-1 && ptr!=NULL; i++)
+    {
+        ptr = ptr->next;
+    }
+    if(ptr!=NULL)
+    {
+        return ptr->data;
+    }
+    else
+    {
+        return -1;
+    }
+}
+int main(){
+    top = push(top, 78);
+    top = push(top, 7);
+    top = push(top, 8);
+    
+    linkedListTraversal(top);
+    int i = 2;
+    printf("Value at position %d is, %d", i , peek(i));
+    //int element = pop(top); 
+    //printf("Popped element is %d\n", element);
+    //linkedListTraversal(top);
+
+    return 0;
+}
